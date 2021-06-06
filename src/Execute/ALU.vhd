1831b3cd950dc3da5090a9d5265874a88f141ede
AhmedMGZ7 <48853566+AhmedMGZ7@users.noreply.github.com>
2021-06-06 18:42:11 +0200

    1 and 2 operands tests
    assembler still is not used and load use is not implemented , ALU fixes

--- src/Execute/ALU.vhd
+++ src/Execute/ALU.vhd
@@ -8,6 +8,7 @@ entity ALU is
     generic (n :integer := 32);
     port (
         rsrc, rdst, inport, shift_immediate,sp_in : in std_logic_vector(n-1 downto 0);
+        flag_reg : in std_logic_vector(2 downto 0);
         opCode: in std_logic_vector (5 downto 0);
         result,sp_new : out std_logic_vector (n-1 downto 0);
         fcout, fneg, fzero: out std_logic
@@ -17,7 +18,7 @@ end ALU;
 architecture ALUarc of ALU is
   signal temp_cout, temp_neg, temp_zero:  std_logic;
   signal shift_left_result,shift_right_result,add_result,sub_result,iadd_result : std_logic_vector (n downto 0);
-  signal rsrc_tow_complement, temp_result : std_logic_vector (n-1 downto 0);
+  signal rsrc_tow_complement,rdst_tow_complement, temp_result : std_logic_vector (n-1 downto 0);
 
     begin
 
@@ -35,14 +36,21 @@ architecture ALUarc of ALU is
       else (inport) when (opCode(5 downto 0) = "001101") -- IN rdst
       else (rsrc) when (opCode(5 downto 0) = "010000") -- MOV rdst
       else (add_result(n-1 downto 0)) when (opCode(5 downto 0) = "010010") -- Add rdst
+      else (add_result(n-1 downto 0)) when (opCode(5 downto 0) = "010011") -- Add rdst
       else (sub_result(n-1 downto 0)) when (opCode(5 downto 0) = "010100") -- SUB rdst
+      else (sub_result(n-1 downto 0)) when (opCode(5 downto 0) = "010101") -- SUB rdst
       else (rdst and rsrc) when (opCode(5 downto 0) = "010110") -- AND rdst
+      else (rdst and rsrc) when (opCode(5 downto 0) = "010111") -- AND rdst
       else (rdst or rsrc) when (opCode(5 downto 0) = "011000") -- OR rdst
+      else (rdst or rsrc) when (opCode(5 downto 0) = "011001") -- OR rdst
       else (rdst+shift_immediate) when (opCode(5 downto 0) = "011010") -- IAdd rdst
+      else (rdst+shift_immediate) when (opCode(5 downto 0) = "011011") -- IAdd rdst
       else (shift_left_result(n-1 downto 0)) when (opCode(5 downto 0) = "011100") -- SHL rdst
+      else (shift_left_result(n-1 downto 0)) when (opCode(5 downto 0) = "011101") -- SHL rdst
       else (shift_right_result(n downto 1)) when (opCode(5 downto 0) = "011110") -- SHR rdst
-      else (rdst(30 downto 0)&temp_cout) when (opCode(5 downto 0) = "001010") -- RLC rdst
-      else (temp_cout & rdst(31 downto 1)) when (opCode(5 downto 0) = "001011") -- RRC rdst
+      else (shift_right_result(n downto 1)) when (opCode(5 downto 0) = "011111") -- SHR rdst
+      else (rdst(30 downto 0)&flag_reg(0)) when (opCode(5 downto 0) = "001010") -- RLC rdst
+      else (flag_reg(0) & rdst(31 downto 1)) when (opCode(5 downto 0) = "001011") -- RRC rdst
       else (sp_in) when (opCode(5 downto 0) = "100000") -- push rdst
       else (sp_in+2) when (opCode(5 downto 0) = "100010") -- pop rdst
       else (shift_immediate) when (opCode(5 downto 0) = "100100") -- LDM rdst
@@ -55,13 +63,15 @@ architecture ALUarc of ALU is
       else (others => '0');
 
       -- the shift left and right
-      lblShiftLef: shift_left_result <=  std_logic_vector(shift_left(unsigned('0' & rsrc), to_integer(unsigned(shift_immediate))));
-      lblShiftRight: shift_right_result <=  std_logic_vector(shift_right(unsigned(rsrc & '0'), to_integer(unsigned(shift_immediate))));
+      lblShiftLef: shift_left_result <=  std_logic_vector(shift_left(unsigned('0' & rdst), to_integer(unsigned(shift_immediate))));
+      lblShiftRight: shift_right_result <=  std_logic_vector(shift_right(unsigned(rdst & '0'), to_integer(unsigned(shift_immediate))));
 
       -- add and subtract
-      rsrc_tow_complement <= not(rsrc)+1;
+      -- rsrc_tow_complement <= not(rsrc)+1;
+      rdst_tow_complement <= not(rdst)+1;
       add_result <= '0' & rdst + rsrc;
-      sub_result <= '0' & rdst + rsrc_tow_complement;
+      -- sub_result <= '0' & rdst + rsrc_tow_complement;
+      sub_result <= '0' & rsrc + rdst_tow_complement;
       iadd_result <= '0' & rdst + shift_immediate;
       
       -- zero flag
@@ -71,7 +81,7 @@ architecture ALUarc of ALU is
 
       -- calculating the carry
       fcout <= temp_cout;
-      lblCarry: temp_cout <= ( '0' ) when (opCode(5 downto 0) = "000000") -- NOP
+      lblCarry: temp_cout <= flag_reg(0) when (opCode(5 downto 0) = "000000") -- NOP
       else ('1') when (opCode(5 downto 0) = "000001") -- setC
       else ('0') when (opCode(5 downto 0) = "000010") -- CLRC
       else ('0') when (opCode(5 downto 0) = "001111") -- CLR rdst
@@ -99,7 +109,7 @@ architecture ALUarc of ALU is
       else ( '0');
 
       -- calculating the zero
-      lblZero: fzero <= ( '0' ) when (opCode(5 downto 0) = "000000") -- NOP
+      lblZero: fzero <= flag_reg(2) when (opCode(5 downto 0) = "000000") -- NOP
       else ('0') when (opCode(5 downto 0) = "000001") -- setC
       else ('0') when (opCode(5 downto 0) = "000010") -- CLRC
       else ('1') when (opCode(5 downto 0) = "001111") -- CLR rdst
@@ -127,7 +137,7 @@ architecture ALUarc of ALU is
       else ( '0');
 
       -- calculating the negative
-      lblNeg: fneg <= ( '0' ) when (opCode(5 downto 0) = "000000") -- NOP
+      lblNeg: fneg <= flag_reg(1) when (opCode(5 downto 0) = "000000") -- NOP
       else ('0') when (opCode(5 downto 0) = "000001") -- setC
       else ('0') when (opCode(5 downto 0) = "000010") -- CLRC
       else ('0') when (opCode(5 downto 0) = "001111") -- CLR rdst
